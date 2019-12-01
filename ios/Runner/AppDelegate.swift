42475e9fe2950556a6173fff1ca630e5d0e7064d
aguerra95ugas <aguerra95ugas@gmail.com>
2019-12-01 04:20:06 -0500

    Agregando google maps a aplicación

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,8 @@
 import UIKit
 import Flutter
 
+import GoogleMaps
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
@@ -8,6 +10,7 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    GMSServices.provideAPIKey("AIzaSyCIKTC5tGWpgAms2PaHco4yE32SemOG2JQ")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
